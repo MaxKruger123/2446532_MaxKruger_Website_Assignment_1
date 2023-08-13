5b7a182d73269ae3faca4d916ec36f964d622cc5
Max <maxkruger321@gmail.com>
2023-08-13 18:31:28 +0200

    Finished blogs page

--- JS/blogs.js
+++ JS/blogs.js
@@ -2,4 +2,13 @@ function ChangeToHomePage() {
 
     window.location.replace("index.html");
     
-    }
+}
+
+function ChangeToBlog1(){
+    window.location.replace("blog1.html");
+}
+
+function ChangToBlogPosts(){
+    window.location.replace("Blogs.html");
+}
+
