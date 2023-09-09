7533a0a656292af1b383ac145ad56dc642fb0703
Max <maxkruger321@gmail.com>
2023-09-09 19:17:26 +0200

    Fixed nav system and other fixes

--- JS/Scroll.js
+++ JS/Scroll.js
@@ -1,13 +1,25 @@
-let mybutton = document.getElementById("ScrollToTop");
+document.addEventListener('DOMContentLoaded', function() {
+  // Get a reference to the button element by its ID
+  const scrollToTopButton = document.getElementById('ScrollToTop');
+
+  // Add a click event listener to the button
+  scrollToTopButton.addEventListener('click', function() {
+    // Call your scroll-to-top function here
+    topFunction();
+  });
+
+  // Your other DOM manipulation and event listeners can go here as well
+});
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  const scrollToTopButton = document.getElementById('ScrollToTop');
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    scrollToTopButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    scrollToTopButton.style.display = "none";
   }
 }
 
@@ -16,3 +28,4 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
---
+++ JS/nav.js
@@ -0,0 +1,23 @@
+const navigationData = [
+    { name: 'HomePlanet', url: 'index.html' },
+    { name: 'The Blog hole', url: 'Blogs.html' },
+    { name: 'Designing Mars', url: 'Design.html' },
+   
+  ];
+
+  const navigation = document.getElementById('navigation');
+  navigationData.forEach(page => {
+  const link = document.createElement('a');
+  link.href = page.url;
+  link.textContent = page.name;
+  navigation.appendChild(link);
+});
+
+const currentPageURL = window.location.href;
+const navigationLinks = document.querySelectorAll('#navigation a');
+
+navigationLinks.forEach(link => {
+  if (link.href === currentPageURL) {
+    link.classList.add('Active'); 
+  }
+});
