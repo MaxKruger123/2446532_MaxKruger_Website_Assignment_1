bdceb6021a42738c8031fca195566c3ef6ab6d2e
MaxKruger123 <maxkruger321@gmail.com>
2023-09-11 00:46:19 +0200

    Created First Scatter Plot

--- JS/Visualization.js
+++ JS/Visualization.js
@@ -1,54 +1,71 @@
- // Fetch data from the API
- fetch('https://api.nasa.gov/insight_weather/?api_key=rJrm96uIegrcAokW4gwHWXgSFcvGnVHmrxOonrSf&feedtype=json&ver=1.0')
- .then(response => response.json())
- .then(data => {
-     // Extract sol (day) and temperature data
-     const solData = data.sol_keys;
-     const tempData = solData.map(sol => data[sol].AT.av); // AT.av is the average temperature
+fetch ("https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=rJrm96uIegrcAokW4gwHWXgSFcvGnVHmrxOonrSf")
+.then((r) => r.json())
+.then(data => {
+    console.log(data); // logs the data file
 
-     const areNumerical = tempData.every(value => !isNaN(value));
+    const neoData = data.near_earth_objects["2015-09-07"];
 
-     // Create the scatter plot
-     const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-     const width = 400 - margin.left - margin.right;
-     const height = 300 - margin.top - margin.bottom;
+    const names = neoData.map((neo) => neo.name);
+    const diameter = neoData.map((neo) => neo.estimated_diameter.meters.estimated_diameter_max);
 
-     const svg = d3.select("#chart")
-         .append("svg")
-         .attr("width", width + margin.left + margin.right)
-         .attr("height", height + margin.top + margin.bottom)
-         .append("g")
-         .attr("transform", `translate(${margin.left},${margin.top})`);
+    let cometYear =[
+        {x: 2002, y: diameter[0]},
+        {x: 2015, y: diameter[1]},
+        {x: 2015, y: diameter[2]},
+        {x: 2015, y: diameter[3]},
+        {x: 2015, y: diameter[4]},
+        {x: 2015, y: diameter[5]},
+        {x: 2015, y: diameter[6]},
+        {x: 2016, y: diameter[7]},
+        {x: 2016, y: diameter[8]},
+        {x: 2018, y: diameter[9]},
+        {x: 2019, y: diameter[10]},
+        {x: 2020, y: diameter[11]},
+        {x: 2020, y: diameter[12]},
 
-     // Define scales for X and Y axes
-     const x = d3.scaleLinear()
-         .domain([0, solData.length - 1])
-         .range([0, width]);
+    ]
 
-     const y = d3.scaleLinear()
-         .domain([Math.min(...tempData), Math.max(...tempData)])
-         .range([height, 0]);
+    console.log(cometYear[8]);
 
-     // Create X and Y axes
-     svg.append("g")
-         .attr("class", "x-axis")
-         .attr("transform", `translate(0,${height})`)
-         .call(d3.axisBottom(x));
+    let margin = 0;
+    let topMargin = 460;
+    let vizArea = d3.select('#Viz')
+    let xScale = d3.scaleLinear().domain([2002, 2020]).range([0, 700]);
+    let yScale = d3.scaleLinear().domain([0, 712]).range([500, 0]);
 
-     svg.append("g")
-         .attr("class", "y-axis")
-         .call(d3.axisLeft(y));
-
-     // Create circles for the scatter plot
-     svg.selectAll("circle")
-         .data(tempData)
-         .enter()
-         .append("circle")
-         .attr("cx", (d, i) => x(i))
-         .attr("cy", d => y(d))
-         .attr("r", 5); // Radius of circles
-
-         // Create Y axis with labels
+    
 
 
- })
+    vizArea
+    .append('g')
+    .attr("transform", `translate( ${margin} , ${460-topMargin} )`)
+    .call(d3.axisRight(yScale));
+    
+    
+    vizArea
+    .append('g')
+    .attr("transform", `translate( ${margin} , 500 )`)
+    .call(d3.axisBottom(xScale));
+
+    vizArea
+    .selectAll('dots')
+    .data(cometYear)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d){
+        return xScale(d.x);
+    })
+    .attr("cy", function(d){
+        return yScale(d.y);
+    })
+    .attr ("r", 5)
+    
+
+
+    console.log(names);
+    console.log(diameter);
+})
+.catch(error => {
+    console.error('Cant fetch data', error);
+});
+
